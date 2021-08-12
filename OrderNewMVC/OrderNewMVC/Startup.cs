c3978574322a3b2061c15bb8ded6795c74fcf3b8
mohammeddagga <dagga-mohammed@hotmail.com>
2021-08-12 10:58:46 +0300

    qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqsad

---
+++ OrderNewMVC/OrderNewMVC/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OrderNewMVC.Startup))]
+namespace OrderNewMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+        }
+    }
+}
